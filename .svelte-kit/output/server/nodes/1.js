5d27cb63c8962de11f5910385de2dbec0a6653c4
Giacomo Nanni <gn.nanni@gmail.com>
2026-01-14 16:08:28 +0100

    chore: remove build artifacts from tracking

--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.CX0F6pyj.js","_app/immutable/chunks/BArpSiJ7.js","_app/immutable/chunks/C9FWihKz.js","_app/immutable/chunks/0waum1Sv.js","_app/immutable/chunks/Yzwz9Dcx.js","_app/immutable/chunks/BY7w2Zq6.js","_app/immutable/chunks/C45ZWEa1.js","_app/immutable/chunks/BP7RbBvP.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.Ch9T7Mic.js","_app/immutable/chunks/BArpSiJ7.js","_app/immutable/chunks/C9FWihKz.js","_app/immutable/chunks/0waum1Sv.js","_app/immutable/chunks/BP7RbBvP.js","_app/immutable/chunks/BvFvupHY.js","_app/immutable/chunks/BY7w2Zq6.js","_app/immutable/chunks/Yzwz9Dcx.js"];
-export const stylesheets = ["_app/immutable/assets/2.9e_zEQBr.css"];
-export const fonts = [];
